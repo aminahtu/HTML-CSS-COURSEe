fed6677582b65b4726a0eab1b3279e4196c9e473
aminahtu <aminahtuabu@gmail.com>
2026-09-10 13:47:48 +0100

    a new html file added and js code

--- HTML-CSS-COURSE/styles/index.js
+++ HTML-CSS-COURSE/styles/index.js
@@ -1,48 +1,67 @@
-function rockPaperScissors () {
-    console.log("Getting started on Game");
-    const userChoicePrompt = prompt ("Enter Rock, Paper, Scissors")
-    const userChoice = userChoicePrompt.toLowerCase();
+// function rockPaperScissors () {
+//     console.log("Getting started on Game");
+//     const userChoicePrompt = prompt ("Enter Rock, Paper, Scissors")
+//     const userChoice = userChoicePrompt.toLowerCase();
 
-    let computerChoice;
+//     let computerChoice;
 
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
+//     const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-if (randomNumber === 1) {
-    computerChoice = "rock";
-} else if (randomNumber === 2) {
-    computerChoice = "paper";
-} else { 
-    computerChoice = "scissors";
-}
+// if (randomNumber === 1) {
+//     computerChoice = "rock";
+// } else if (randomNumber === 2) {
+//     computerChoice = "paper";
+// } else { 
+//     computerChoice = "scissors";
+// }
 
-    console.log("user selected", userChoice);
-    console.log("computer selected", computerChoice);
+//     console.log("user selected", userChoice);
+//     console.log("computer selected", computerChoice);
 
-    if (
-        (userChoice === "rock" && computerChoice === "scissors" ) ||
-        (userChoice === "paper" && computerChoice === "rock") ||
-        (userChoice === "scissors" && computerChoice === "paper")
-    ) {
-        console.log("The user wins !!!")
-    } else if (
-        (userChoice === computerChoice) ){
-        console.log("It is a tie")
-    } else if (
-        (userChoice === "scissors" && computerChoice === "rock") ||
-        (userChoice === "rock" && computerChoice === "paper") ||
-        (userChoice === "paper" && computerChoice === "scissors")
-    ) {
-        console.log("The computer wins")
-    } else {
-        console.log("Please check the input")
+//     if (
+//         (userChoice === "rock" && computerChoice === "scissors" ) ||
+//         (userChoice === "paper" && computerChoice === "rock") ||
+//         (userChoice === "scissors" && computerChoice === "paper")
+//     ) {
+//         console.log("The user wins !!!")
+//     } else if (
+//         (userChoice === computerChoice) ){
+//         console.log("It is a tie")
+//     } else if (
+//         (userChoice === "scissors" && computerChoice === "rock") ||
+//         (userChoice === "rock" && computerChoice === "paper") ||
+//         (userChoice === "paper" && computerChoice === "scissors")
+//     ) {
+//         console.log("The computer wins")
+//     } else {
+//         console.log("Please check the input")
+//     }
+
+//     const playAgainPrompt = prompt("Do you want to Play Again? (yes/no)")
+//     const playAgain = playAgainPrompt ? playAgainPrompt.toLowerCase() : "no";
+
+// if (playAgain === "yes") {
+//     rockPaperScissors ();
+// } else {
+//     console.log("Thanks for Playing! see you Again")
+// }
+// }
+
+const faq = document.querySelector(".faq");
+
+
+faq.addEventListener("click", (e) => {
+    if (e.target.classList.contains("question")) {
+        e.stopPropagation();
+    const currentItem = e.target.parentElement;
+    const currentAnswer = currentItem.querySelector(".answer");
+
+    currentAnswer.classList.toggle("show");
     }
+})
 
-    const playAgainPrompt = prompt("Do you want to Play Again? (yes/no)")
-    const playAgain = playAgainPrompt ? playAgainPrompt.toLowerCase() : "no";
+document.addEventListener("click", function() {
+    const allAnswers = document.querySelectorAll(".answer.show")
 
-if (playAgain === "yes") {
-    rockPaperScissors ();
-} else {
-    console.log("Thanks for Playing! see you Again")
-}
-}
+    allAnswers.forEach(answer => answer.classList.remove("show"))
+})
